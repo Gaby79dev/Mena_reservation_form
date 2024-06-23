bfb8b80895ebe47c436f69ef192432223daecbe5
Gaby79dev <gabrielvisiedopelaez@gmail.com>
2024-06-23 14:45:08 +0200

    Actualización plantilla hotel

--- mena_reservation_form/mena_reservation_form.py
+++ mena_reservation_form/mena_reservation_form.py
@@ -1,45 +1,37 @@
-from rxconfig import config
-from .components.navbar import navbar
-from .components.header import header
 
 import reflex as rx
+from rxconfig import config
+from .components.navbar import navbar
+from .pages.reservation_form import reservationForm
+from .pages.taxi_reservation import taxiReservation
+from .pages.confirmation_screen import confirmation_screen
+from .components.footer import footer
 
-@rx.page(title="Hotel Mena reservation form", image="/favicon.ico")
+
+@rx.page(title="Hotel Mena Reservation Form", image="/favicon.ico")
 def index() -> rx.Component:
     return rx.flex(
         rx.vstack(
             navbar(),
             rx.spacer(spacing='4'),
-            header(),
-            rx.spacer(spacing='9'),
-            rx.hstack(
-                rx.text(
-                    "© Developed by VipeDev with ",
-                    size="2",                
-                ),
-                rx.link(
-                    rx.image(src="https://reflex.dev/logos/light/reflex.svg", 
-                    width="60px"),
-                    href="https://reflex.dev/",
-                ),
-                align="center",
-                margin_bottom="12px"
-            ),
-            align="center",
-            width="100%",
-            height="100%",
-            flex_grow=1,
-        ),
+            reservationForm(),
+            footer(),
+            
         width="100%",
-        height="100vh",  # Ajuste para ocupar toda la altura de la ventana.
-        bg="#F2F4F7",
-        flex_grow=1,  # Añadido para que el contenedor flex crezca y ocupe el espacio disponible.
+        bg="#E7EEF7",
+        align="center"
+        )
     )
 
 app = rx.App(
     theme=rx.theme(
         has_background=True,
-        bg="#F2F4F7"
+        
     )
 )
-app.add_page(index)
+
+app.add_page(index, route=('/'))
+app.add_page(taxiReservation, route=('/taxi_reservation'))
+app.add_page(confirmation_screen, route=('/confirmation_screen'))
+
+app._compile()
